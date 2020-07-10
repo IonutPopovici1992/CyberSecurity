1349c27ee4bdcfebae81f619724568ee7b42ebd6
Ionut Popovici <ionut_popovici92@yahoo.com>
2020-07-10 23:58:25 +0300

    Cyber Security: Python - Variables

---
+++ Python/variables.py
@@ -0,0 +1,31 @@
+# print()
+
+print("Python Rocks!!!")
+print('Hello, World!!!')
+print('Hello, my name is "Alexis" and I love Python.')
+
+print(10 * "-")
+
+# Mathematical Operators
+
+print(1 + 2)
+print(1 - 2)
+print(1 * 2)
+print(1 / 2)
+print(1 % 2)
+
+print(10 * "-")
+
+# Variables
+
+age = 20
+name = "Alexis"
+name = "John Wick"
+print(age + 45)
+print(name)
+
+print("This is a simple calculator.")
+a = 10
+b = 20
+sum = a + b
+print(sum)
