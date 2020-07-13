3f878ea0d1d288c8cd8e6aeb563669d5645ade24
Ionut Popovici <ionut_popovici92@yahoo.com>
2020-07-13 11:27:47 +0300

    Cyber Security: Python - Lists

---
+++ Python/lists.py
@@ -0,0 +1,22 @@
+# Lists
+
+numbers = [23, 34, 67, 87, 102]
+# print(numbers)
+
+numbers[1] = 456
+print(numbers)
+
+# numbers + [39, 321]
+# print(numbers)
+
+# append()
+numbers.append(120)
+print(numbers)
+
+print(numbers[:3])
+
+numbers[:4] = [2, 4, 6, 8]
+print(numbers)
+
+numbers[:] = []
+print(numbers)
