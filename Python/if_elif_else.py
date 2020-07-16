d68aaf999affdec4ca9082a832d72b2df7a68266
Ionut Popovici <ionut_popovici92@yahoo.com>
2020-07-16 09:44:02 +0300

    Cyber Security: Python - if, elif, else

---
+++ Python/if_elif_else.py
@@ -0,0 +1,21 @@
+# if, elif, else
+
+"""
+marks = 50
+
+if marks > 80:
+    print("You have passed!")
+elif marks < 40:
+    print("You have failed!")
+else:
+    print("You performed averagely, try again harder.")
+"""
+
+name = "Alexis"
+
+if name is "Alexis":
+    print("Welcome back!")
+elif name is "John":
+    print("You are not an admin!")
+else:
+    print("You do not have an account, please create one!")
