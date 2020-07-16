16f493f5891be3aec2d2535a7e49dfcef6ab23e8
Ionut Popovici <ionut_popovici92@yahoo.com>
2020-07-16 11:03:42 +0300

    Cyber Security: Python - Dictionary

---
+++ Python/dictionary.py
@@ -0,0 +1,6 @@
+# Dictionary
+
+dictionary_name = {'Alexis': 'Ethical Hacker', 'Joel': 'Ethical Character', 'John': 'Ethical Spy'}
+
+for key, value in dictionary_name.items():
+    print(key + " : " + value)
