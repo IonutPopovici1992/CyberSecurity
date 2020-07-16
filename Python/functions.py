c0878f72255b2061011908a5f11638c0b76fdf27
Ionut Popovici <ionut_popovici92@yahoo.com>
2020-07-16 11:21:11 +0300

    Cyber Security: Python - Functions

---
+++ Python/functions.py
@@ -0,0 +1,17 @@
+# Functions
+
+"""
+def function():
+    print("This is a function.")
+
+
+function()
+"""
+
+
+def currency_converter(usd):
+    amount = usd * 0.76
+    print(amount)
+
+
+currency_converter(200)
