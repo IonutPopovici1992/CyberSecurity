6a3704eaa645ef3fb28744b599bffa6f32d16a6c
Ionut Popovici <ionut_popovici92@yahoo.com>
2020-07-16 10:16:13 +0300

    Cyber Security: Python - for loop

---
+++ Python/for_loop.py
@@ -0,0 +1,14 @@
+# for
+
+"""
+numbers = [34, 13, 78, 110, 67, 86]
+
+for number in numbers:
+    print(number)
+"""
+
+sports = ['Football', 'Tennis', 'Golf', 'Polo']
+
+for sport in sports:
+    print(sport)
+    print(len(sport))
