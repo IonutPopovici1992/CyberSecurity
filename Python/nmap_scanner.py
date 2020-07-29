bf69306c52aab13f3490e58c51321b6b81cf2c7e
Ionut Popovici <ionut_popovici92@yahoo.com>
2020-07-29 16:19:59 +0300

    Cyber Security: Python - Nmap Scanner Part 2

--- Python/nmap_scanner.py
+++ Python/nmap_scanner.py
@@ -6,8 +6,20 @@ ns = nmap.PortScanner()
 
 print(ns.nmap_version())
 
-ns.scan('192.168.177.128', '1-1024', '-v')
+ns.scan('192.168.177.128', '1-1024', '-v --version-all')
 
-print(ns.scaninfo())
+# print(ns.scaninfo())
 
-print(ns.csv())
+# print(ns.csv())
+
+print(ns.scanstats())
+
+# print(ns.all_hosts())
+
+# print(ns['192.168.177.128'].state())
+
+# print(ns['192.168.177.128'].all_protocols())
+
+# print(ns['192.168.177.128']['tcp'].keys())
+
+print(ns['192.168.177.128'].has_tcp(80))
